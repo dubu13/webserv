2d270d3f4288fe5d6b6663dd95011f03b032fe95
dhasan <dilara.hasan1301@gmail.com>
2025-05-22 21:11:42 +0200

    refactor: implement config file parsing system
    - move parsing to respective classes
    - implement directive handlers for server and location blocks

--- inc/config/ServerConfig.hpp
+++ inc/config/ServerConfig.hpp
@@ -3,20 +3,39 @@
 #include <map>
 #include <optional>
 #include <string>
-#include <vector>
-
+#include <unordered_map>
+#include <sstream>
+#include <fstream>
+#include <stdexcept>
+#include "LocationConfig.hpp"
 class ServerConfig {
-public:
-  std::string host;
-  int port;
-  std::string server_name;
-  std::string root;
-  std::map<int, std::string> error_pages;
-  size_t client_max_body_size;
-  std::map<std::string, LocationConfig> locations;
+  private:
+      enum class ServerDirective {
+        PORT,
+        HOST,
+        SERVER_NAME,
+        ROOT,
+        ERROR_PAGES,
+        CLIENT_MAX_BODY_SIZE,
+        LOCATION,
+        UNKNOWN
+    };
+    
+    std::unordered_map<std::string, ServerDirective> _serverDirectives;
+  
+  public:
+      std::string host;
+      int port;
+      std::string server_name;
+      std::string root;
+      std::map<int, std::string> error_pages;
+      size_t client_max_body_size;
+      std::map<std::string, LocationConfig> locations;
 
-  ServerConfig();
-
-  bool matchesHost(const std::string &host) const;
-  const LocationConfig *getLocation(const std::string &path) const;
+      // bool matchesHost(const std::string &host) const;
+      // const LocationConfig *getLocation(const std::string &path) const;
+      
+      ServerConfig();
+      void parseServerBlock(std::ifstream &file);
+      void handleServerDirective(ServerDirective type, std::istringstream& iss);
 };
--- src/config/ServerConfig.cpp
+++ src/config/ServerConfig.cpp
@@ -2,21 +2,96 @@
 
 ServerConfig::ServerConfig()
     : host("localhost"), port(8080), root("./www"),
-      client_max_body_size(1024 * 1024) {} // 1 MB default
+      client_max_body_size(1024 * 1024) {
 
-bool ServerConfig::matchesHost(const std::string &host) const {
-  if (server_names.empty()) {
-    return true; // If no server names are specified, match all
+        _serverDirectives = {
+          {"port", ServerDirective::PORT},
+          {"host", ServerDirective::HOST},
+          {"server_name", ServerDirective::SERVER_NAME},
+          {"root", ServerDirective::ROOT},
+          {"error_pages", ServerDirective::ERROR_PAGES},
+          {"client_max_body_size", ServerDirective::CLIENT_MAX_BODY_SIZE},
+          {"location", ServerDirective::LOCATION}
+      };
+} // 1 MB default
+
+// bool ServerConfig::matchesHost(const std::string &host) const {
+//   if (server_name.empty()) {
+//     return true; // If no server names are specified, match all
+//   }
+//   return std::find(server_names.begin(), server_names.end(), host) !=
+//          server_names.end();
+// }
+
+// const LocationConfig *ServerConfig::getLocation(const std::string &path) const {
+//   for (const auto &location : locations) {
+//     if (path.find(location.first) == 0) {
+//       return &location.second;
+//     }
+//   }
+//   return nullptr; // No matching location found
+// }
+
+
+void ServerConfig::parseServerBlock(std::ifstream &file) {
+  std::string line;
+
+  while (std::getline(file, line)) {
+      if (line.empty() || line[0] == '#')
+          continue;
+      if (line == "}")
+          return;
+       std::istringstream iss(line);
+       std::string directive;
+       iss >> directive;
+
+       auto it = _serverDirectives.find(directive);
+       ServerDirective type = (it != _serverDirectives.end()) ? it->second : ServerDirective::UNKNOWN;
+
+       switch(type) {
+          case ServerDirective::LOCATION: {
+              std::string path;
+              iss >> path;
+              LocationConfig locationBlock;
+              locationBlock.parseLocationBlock(file);
+              locations[path] = locationBlock;
+              break;
+          }
+          case ServerDirective::UNKNOWN:
+              throw std::runtime_error("Unknown directive: " + directive);
+          default:
+              handleServerDirective(type, iss);
+       }
   }
-  return std::find(server_names.begin(), server_names.end(), host) !=
-         server_names.end();
 }
 
-const LocationConfig *ServerConfig::getLocation(const std::string &path) const {
-  for (const auto &location : locations) {
-    if (path.find(location.first) == 0) {
-      return &location.second;
-    }
+void ServerConfig::handleServerDirective(ServerDirective type, std::istringstream& iss) {
+  std::string value;
+
+  switch(type) {
+      case ServerDirective::PORT:
+          iss >> port;
+          break;
+      case ServerDirective::HOST:
+          iss >> host;
+          break;
+      case ServerDirective::SERVER_NAME:
+          iss >> server_name;
+          break;
+      case ServerDirective::ROOT:
+          iss >> root;
+          break;
+      case ServerDirective::ERROR_PAGES: {
+          int error_code;
+          std::string path;
+          iss >> error_code >> path;
+          error_pages[error_code] = path;
+          break;
+      }
+      case ServerDirective::CLIENT_MAX_BODY_SIZE:
+          iss >> client_max_body_size;
+          break;
+      default:
+          throw std::runtime_error("Unknown directive");
   }
-  return nullptr; // No matching location found
 }
